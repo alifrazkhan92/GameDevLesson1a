c60300269df29b9f4ce69728f3a2d0ddb3945392
Ali Fraz <alifrazkhan92@gmail.com>
2023-11-14 16:39:13 +0000

    Initial commit

---
+++ HelloSprite.py
@@ -0,0 +1,49 @@
+import pygame
+from pygame.locals import *
+pygame.init()
+screen = pygame.display.set_mode((800, 600), 0, 32)
+sprite1 = pygame.image.load('./images/butterfly.png')
+sprite1 = pygame.transform.scale(sprite1, (50, 50))
+spriteWidth = sprite1.get_width()
+spriteHeight = sprite1.get_height()
+pygame.display.set_caption("Hello Sprite")
+screen.fill((0, 0, 0))
+
+game_over = False
+x, y = 0, 0
+clock = pygame.time.Clock()
+while not game_over:
+    dt = clock.tick(100)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+        elif event.type == pygame.MOUSEMOTION:
+            x, y = event.pos
+            x -= spriteWidth/2
+            y -= spriteHeight/2
+    #screen.blit(sprite1, (screen.get_width()/2 - spriteWidth/2, screen.get_height()/2 - spriteHeight/1))
+    pressed = pygame.key.get_pressed()
+    if pressed[K_UP]:
+        y -= 0.5 * dt
+    if pressed[K_DOWN]:
+        y += 0.5 * dt
+    if pressed[K_RIGHT]:
+        x += 0.5 * dt
+    if pressed[K_LEFT]:
+        x -= 0.5 * dt
+    if pressed[K_SPACE]:
+        x = 0
+        y = 0
+
+    if x > (screen.get_width() - spriteWidth):
+        x = screen.get_width() - spriteWidth
+    if y > (screen.get_height() - spriteHeight):
+        y = screen.get_height() - spriteHeight
+    if x < 0:
+        x = 0
+    if y < 0:
+        y = 0
+    screen.fill((0, 0, 0))
+    screen.blit(sprite1, (x, y))
+    pygame.display.update()
+pygame.quit()
